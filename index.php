e378bb8c1d4e0446b3a3f0ceea27dfdb52fab253
gioanbatan <giovannibassani@hotmail.com>
2022-11-24 16:04:23 +0100

    Add: colors and style

--- index.php
+++ index.php
@@ -17,15 +17,12 @@
         <p>Inserisci un testo e la parola da censurare</p>
 
         <form action="result.php" method="GET">
-            <label for="text">Testo da sottoporre a censura:
-            </label>
+            <label for="text">Testo da sottoporre a censura:</label>
             <textarea id="text" name="text" rows="5" cols="40" placeholder="Inserisci il testo qui..."></textarea>
-            
-            <br>
             
             <label for="word">Inserisci la parola da censurare:</label>
             <input type="text" id="word" name="word" placeholder="Inserisci una parola">
-
+            
             <br>
             
             <button type="submit">Invia!</button>
